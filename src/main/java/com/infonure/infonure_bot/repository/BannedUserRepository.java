dde8e85a1b40613d079b43b0e51dfb50b411cb29
iden8 <66456149+iden8@users.noreply.github.com>
2025-06-14 12:32:40 +0300

    Delete src directory

--- src/main/java/com/infonure/infonure_bot/repository/BannedUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infonure.infonure_bot.repository;
-
-import com.infonure.infonure_bot.model.BannedUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BannedUserRepository extends JpaRepository<BannedUser, Long> {
-}
